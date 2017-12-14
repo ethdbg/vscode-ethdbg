967eea25ffb50480bbe591deb6eafcf9f1272ba6
Andrew Plaza <aplaza@liquidthink.net>
2017-12-14 00:39:52 -0500

    add provideInitialConfigurations command and make extension.ts make sense

---
+++ src/activationEvents.ts
@@ -0,0 +1,28 @@
+'use strict';
+import * as vscode from 'vscode';
+
+/** activation events must mirror package.json vscode-ext debugger schema */
+
+const initialConfigurations = {
+  version: '0.0.1',
+  configurations: [
+  {
+    type: 'ether-debug',
+    request: 'launch',
+    program: '${file}',
+    stopOnEntry: true,
+  }
+]}
+
+export function getProgramName(config) {
+  return vscode.window.showInputBox({
+    placeHolder: "Enter contract name",
+    value: "contract.sol"
+  });
+}
+
+export function provideInitialConfigurations() {
+  return [
+    JSON.stringify(initialConfigurations, null, '\t')
+  ].join('\n');
+}
--- src/extension.ts
+++ src/extension.ts
@@ -6,33 +6,54 @@
 
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import * as events from './activationEvents';
+
+
+
+
+
+
 
 export function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.ether-debug.getProgramName', config => {
-		return vscode.window.showInputBox({
-			placeHolder: "Enter contract name",
-			value: "contract.sol"
-		});
-	}));
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'extension.ether-debug.getProgramName', 
+      events.getProgramName
+    )
+  );
 
 	// register a configuration provider for 'ether' debug type
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('ether', new etherConfigurationProvider()));
+  context.subscriptions.push(
+    vscode.debug.registerDebugConfigurationProvider(
+      'ether-debug', new EtherConfigurationProvider()
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'extension.ether-debug.provideInitialConfigurations', 
+      events.provideInitialConfigurations
+    )
+  );
 }
 
 export function deactivate() {
 	// nothing to do
 }
 
-class etherConfigurationProvider implements vscode.DebugConfigurationProvider {
+class EtherConfigurationProvider implements vscode.DebugConfigurationProvider {
 
 	/**
 	 * Massage a debug configuration just before a debug session is being launched,
 	 * e.g. add all missing attributes to the debug configuration.
 	 */
-	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
-
-		// if launch.json is missing or empty
+  resolveDebugConfiguration(
+    folder: WorkspaceFolder | undefined, 
+    config: DebugConfiguration, token?: CancellationToken
+  ) : ProviderResult<DebugConfiguration> {
+    
+    // if launch.json is missing or empty
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
 			if (editor && editor.document.languageId === 'solidity' ) {
